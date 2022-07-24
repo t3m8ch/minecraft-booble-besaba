56f0b250c465598370aa12881b312bdf64125189
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-24 13:06:10 +0400

    feat: Init commit

---
+++ src/main/kotlin/io/github/t3m8ch/minecraftbooblebesaba/MinecraftBoobleBesabaApplication.kt
@@ -0,0 +1,11 @@
+package io.github.t3m8ch.minecraftbooblebesaba
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinecraftBoobleBesabaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MinecraftBoobleBesabaApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/t3m8ch/minecraftbooblebesaba/MinecraftBoobleBesabaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.t3m8ch.minecraftbooblebesaba
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinecraftBoobleBesabaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
